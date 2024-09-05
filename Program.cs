c8ab3ef1bf2c9b144fa2df21d38b4ce3afbc4616
339610248 <339610248@2260I-L29-023>
2024-09-05 09:29:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Day_2_Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string favMovie;
+            firstName = "rayj";
+            favMovie = "The Matrix";
+            Console.WriteLine($"Hello Everyone, my name is {firstName} and my favorite movie is {favMovie}".ToLower);
+        }
+    }
+}
